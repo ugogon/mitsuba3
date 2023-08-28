5ba95d09a969cdbdaa7680b3f86016f3bd531d00
danielwujecki <daniel.wujecki@gmail.com>
2023-08-28 11:55:05 +0200

    Histogram; acousticpath wavelength_bin handling

--- src/render/histogram.cpp
+++ src/render/histogram.cpp
@@ -2,17 +2,17 @@
 #include <mitsuba/render/histogram.h>
 #include <mitsuba/core/bitmap.h>
 #include <mitsuba/core/profiler.h>
-#include <drjit/loop.h>
+// #include <drjit/loop.h>
 //#include <mitsuba/core/rfilter.h>
 
 NAMESPACE_BEGIN(mitsuba)
 
 MI_VARIANT
 Histogram<Float, Spectrum>::Histogram(const ScalarVector2u &size,
-                                      size_t channel_count,
+                                      uint32_t channel_count,
                                       const ReconstructionFilter *rfilter,
                                       bool border)
-    : m_channel_count((uint32_t) channel_count), m_size(0), m_offset(0), m_rfilter(rfilter), m_weights(nullptr) {
+    : m_channel_count(channel_count), m_size(0), m_offset(0), m_rfilter(rfilter), m_weights(nullptr) {
 
     // TODO: needed?
     // Detect if a box filter is being used, and just discard it in that case
@@ -23,61 +23,53 @@ Histogram<Float, Spectrum>::Histogram(const ScalarVector2u &size,
 
     if (m_rfilter) {
         // Temporary buffers used in put()
-        int filter_size = (int) std::ceil(2 * m_rfilter->radius()) + 1;
+        size_t filter_size = dr::ceil(2 * m_rfilter->radius()) + 1;
         m_weights = new Float[filter_size];
     }
 
     set_size(size);
 }
 
-MI_VARIANT
-Histogram<Float, Spectrum>::~Histogram() {
+MI_VARIANT Histogram<Float, Spectrum>::~Histogram() {
     if (m_weights)
         delete[] m_weights;
 }
 
-MI_VARIANT void
-Histogram<Float, Spectrum>::clear() {
-    size_t total_size = m_channel_count * dr::prod(m_size + 2 * ScalarVector2i(m_border_size, 0));
+MI_VARIANT void Histogram<Float, Spectrum>::clear() {
+    using Array = typename TensorXf::Array;
 
-    if constexpr (!dr::is_jit_v<Float>) {
-        // TODO needed?
-        NotImplementedError("Histogram::clear Scalar case");
-    } else {
-        m_data = dr::zeros<TensorXf>(total_size);
-        m_counts = dr::zeros<TensorXf>(total_size);
-    }
+    ScalarVector2u size_ext = m_size + 2 * ScalarVector2u(m_border_size, 0);
+
+    size_t size_flat = m_channel_count * dr::prod(size_ext),
+           shape[3]  = { size_ext.y(), size_ext.x(), m_channel_count };
+
+    m_tensor = TensorXf(dr::zeros<Array>(size_flat), 3, shape);
+    m_counts = TensorXf(dr::zeros<Array>(size_flat), 3, shape);
 }
 
-MI_VARIANT void
-Histogram<Float, Spectrum>::set_size(const ScalarVector2u &size) {
+MI_VARIANT void Histogram<Float, Spectrum>::set_size(const ScalarVector2u &size) {
+    using Array = typename TensorXf::Array;
+
     if (size == m_size)
         return;
+
+    ScalarVector2u size_ext = size + 2 * m_border_size;
+
+    size_t size_flat = m_channel_count * dr::prod(size_ext),
+           shape[3]  = { size_ext.y(), size_ext.x(), m_channel_count };
+
+    m_tensor = TensorXf(dr::zeros<Array>(size_flat), 3, shape);
+    m_counts = TensorXf(dr::zeros<Array>(size_flat), 3, shape);
+
     m_size = size;
-
-    size_t total_size = m_channel_count * dr::prod(m_size + 2 * ScalarVector2i(m_border_size, 0));
-
-    // Allocate empty buffer
-    // TODO does this work properly?
-    m_data = dr::zeros<TensorXf>(total_size);
-    m_counts = dr::zeros<TensorXf>(total_size);
 }
 
+MI_VARIANT void Histogram<Float, Spectrum>::put_block(const Histogram *hist) { }
 
 MI_VARIANT void
 Histogram<Float, Spectrum>::put(const Point2f &pos,
-                                const Spectrum &value,
+                                const Float *values,
                                 Mask active) {
-}
-
-MI_VARIANT void
-Histogram<Float, Spectrum>::put(const Point2f &pos,
-                                const Float *value,
-                                Mask active) {
-}
-
-MI_VARIANT void
-Histogram<Float, Spectrum>::put(const Histogram * hist) {
 }
 
 MI_IMPLEMENT_CLASS_VARIANT(Histogram, Object)
