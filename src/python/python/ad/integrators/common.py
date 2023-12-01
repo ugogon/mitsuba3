a704211fc8b663ecda47979753434e7cb90c186a
danielwujecki <daniel.wujecki@gmail.com>
2023-12-01 23:25:17 +0100

    graphics prb: warning instead of exception for box filter

--- src/python/python/ad/integrators/common.py
+++ src/python/python/ad/integrators/common.py
@@ -40,7 +40,7 @@ class ADIntegrator(mi.CppADIntegrator):
             raise Exception("\"rr_depth\" must be set to a value greater than zero!")
 
         # Warn about potential bias due to shapes entering/leaving the frame
-        self.sample_border_warning = True
+        self.sample_border_warning = props.get("border_warning", True)
 
     def aovs(self):
         return []
@@ -359,8 +359,10 @@ class ADIntegrator(mi.CppADIntegrator):
         reparam_det = 1.0
 
         if reparam is not None:
-            if rfilter.is_box_filter():
-                raise Exception(
+            if rfilter.is_box_filter() and self.sample_border_warning:
+                self.sample_border_warning = True
+
+                mi.Log(mi.LogLevel.Warn,
                     "ADIntegrator detected the potential for image-space "
                     "motion due to differentiable shape or camera pose "
                     "parameters. This is, however, incompatible with the box "
@@ -948,7 +950,7 @@ class RBIntegrator(ADIntegrator):
             )
 
             # Propagate gradient image to sample positions if necessary
-            if reparam is not None:
+            if True and reparam is not None:
                 with dr.resume_grad():
                     # Accumulate into the image block.
                     # After reparameterizing the camera ray, we need to evaluate
